778e2b87151b48891a58ff934178274d2c9e663c
Khushi <doshikhushi02@gmail.com>
2020-08-25 20:27:31 +0530

    Deleted Unwanted Files

--- app/src/main/java/com/example/quickbuyapp/model/ItemToDisplay.kt
+++
@@ -1,7 +0,0 @@
-package com.example.quickbuyapp.model
-
-data class ItemToDisplay(var title : String,
-                         var image1 : String,
-                         var image2 : String,
-                         var image3: String,
-                         var image4: String)
