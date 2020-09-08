9ffcc4ca08b7590f16034fbaa8dfc7e2afc71418
Khushi <doshikhushi02@gmail.com>
2020-09-08 23:24:31 +0530

    Completed Cancel Order

--- app/src/main/java/com/example/quickbuyapp/ui/viewOrders/ViewOrderFragment.kt
+++ app/src/main/java/com/example/quickbuyapp/ui/viewOrders/ViewOrderFragment.kt
@@ -1,6 +1,8 @@
 package com.example.quickbuyapp.ui.viewOrders
 
 import android.app.AlertDialog
+import android.content.DialogInterface
+import android.graphics.Color
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.view.*
@@ -12,7 +14,10 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.quickbuyapp.Adapter.MyOrderAdapter
 import com.example.quickbuyapp.Callback.ILoadOrderCallbackListener
+import com.example.quickbuyapp.Callback.IMyButtonCallback
 import com.example.quickbuyapp.Common.Common
+import com.example.quickbuyapp.Common.MySwipeHelper
+import com.example.quickbuyapp.EventBus.CountCartEvent
 import com.example.quickbuyapp.R
 import com.example.quickbuyapp.model.Order
 import com.google.firebase.auth.FirebaseAuth
@@ -21,8 +26,15 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import dmax.dialog.SpotsDialog
+import io.reactivex.SingleObserver
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import org.greenrobot.eventbus.EventBus
+import java.lang.StringBuilder
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class ViewOrderFragment : Fragment() , ILoadOrderCallbackListener {
 
@@ -46,7 +58,7 @@ class ViewOrderFragment : Fragment() , ILoadOrderCallbackListener {
 
         viewOrderModel!!.mutableLiveDataOrderList.observe(viewLifecycleOwner , Observer {
             Collections.reverse(it!!)
-            val adapter = MyOrderAdapter(requireContext() , it!!)
+            val adapter = MyOrderAdapter(requireContext() , it!!.toMutableList())
             recycler_order!!.adapter = adapter
         })
 
@@ -90,6 +102,61 @@ class ViewOrderFragment : Fragment() , ILoadOrderCallbackListener {
         val layoutManager = LinearLayoutManager(requireContext())
         recycler_order.layoutManager = layoutManager
         recycler_order.addItemDecoration(DividerItemDecoration(requireContext() , layoutManager.orientation))
+
+
+        val swipe= object : MySwipeHelper(requireContext(),recycler_order!!,200)
+        {
+            override fun instantiateMyButton(
+                viewHolder: RecyclerView.ViewHolder,
+                buffer: MutableList<MyButton>
+            ) {
+                buffer.add(MyButton(context!!,
+                    "Cancel Order",
+                    30,
+                    0,
+                    Color.parseColor("#FF3C30"),
+                    object : IMyButtonCallback {
+                        override fun onClick(pos: Int) {
+                            val orderModel = (recycler_order.adapter as MyOrderAdapter).getItemAtPosition(pos)
+                            if(0 == orderModel.orderStatus){
+                                val builder = androidx.appcompat.app.AlertDialog.Builder(context!!)
+                                builder.setTitle("Cancel Order")
+                                    .setMessage("Do you really want to cancel ?")
+                                    .setNegativeButton("CANCEL"){dialogInterface , i->
+                                        dialogInterface.dismiss()
+                                    }
+                                    .setPositiveButton("YES"){ dialogInterface, i ->
+                                        val update_data = HashMap<String , Any>()
+                                        update_data.put("orderStatus" , -1)
+                                        FirebaseDatabase.getInstance()
+                                            .getReference(Common.ORDER_REF)
+                                            .child(orderModel.orderNumber!!)
+                                            .updateChildren(update_data)
+                                            .addOnFailureListener { e->
+                                                Toast.makeText(context!! , e.message,Toast.LENGTH_SHORT).show()
+                                            }
+                                            .addOnSuccessListener {
+                                                orderModel.orderStatus = -1
+                                                (recycler_order.adapter as MyOrderAdapter).setItemAtPosition(pos , orderModel)
+                                                (recycler_order.adapter as MyOrderAdapter).notifyItemChanged(pos)
+                                                Toast.makeText(context!! , "Cancelled your order Successfully" ,Toast.LENGTH_SHORT).show()
+                                            }
+                                    }
+
+                                val dialog = builder.create()
+                                dialog.show()
+                            }
+                            else{
+                                Toast.makeText(context!! , StringBuilder("Your order status was changed to")
+                                    .append(Common.convertStatusToText(orderModel.orderStatus))
+                                    .append(" , so you can't cancel it"),Toast.LENGTH_SHORT).show()
+                            }
+
+                        }
+                    }))
+            }
+
+        }
     }
 
 
